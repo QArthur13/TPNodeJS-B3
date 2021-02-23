e794323e48ef99336908857e8384ac8c2eb7d926
Quaranta Arthur <arthur.quaranta@ynov.com>
2021-02-23 15:39:22 +0100

    Correction Exo1

--- exo1.js
+++ exo1.js
@@ -1,5 +1,5 @@
 let first_number = "5";
 let second_number = "5";
-let resultat = first_number + second_number;
+let resultat = parseInt(first_number) + parseInt(second_number);
 
-console.log("Le résultat est " + resultat);
+console.log("Le résultat est " + resultat.toString());
